eee49e9ea27ccb0c3e56666b0c43cf433eeb1064
Kenta Fukuhara <105337464+KAkikakuFukuhara@users.noreply.github.com>
2025-03-07 10:17:09 +0900

    [Add] colorize terminal text

---
+++ py3_8_10/terminal/t2_newline.py
@@ -0,0 +1,22 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(f"header{bcolors.HEADER}\n")
+print(f"okblue{bcolors.OKBLUE}\n")
+print(f"okcyan{bcolors.OKCYAN}\n")
+print(f"okgreen{bcolors.OKGREEN}\n")
+print(f"warning{bcolors.WARNING}\n")
+print(f"fail{bcolors.FAIL}\n")
+print(f"endc{bcolors.ENDC}\n")
+print(f"bold{bcolors.BOLD}\n")
+print(f"underline{bcolors.UNDERLINE}\n")
+
+
